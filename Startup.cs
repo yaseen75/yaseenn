efe6980ddc33620ff77abab54e6b39355978d435
yaseen75 <88571712+yaseen75@users.noreply.github.com>
2021-09-03 06:50:39 +0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolSystem.Startup))]
+namespace SchoolSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
